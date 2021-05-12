b46ab66f3d990994650427018ce7d4565990c189
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2021-05-12 17:07:48 +0200

    Jerry:finish the basic part of event and eventlist

---
+++ src/Event/Event.java
@@ -0,0 +1,184 @@
+package Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Event {
+    private String time_create; //when creating this event
+    private String time_start; //when the event starts
+    private String time_end; //when the event ends
+    private Calendar calendarS;
+    private Calendar calendarE;
+    private String title; //the title of the event
+    private String description;
+    private boolean isOnline;
+    private String platform; //if online, choose a platform
+    private String room; //if physical, choose a room (it could be another type, let's see in the future)
+
+    public Event(String title, String description, int yearS, int monthS, int dayS, int hourS, int minuteS,
+                 int yearE, int monthE, int dayE, int hourE, int minuteE, boolean isOnline) {
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+
+        this.time_create = sdf.format(date);
+        this.title = title;
+
+        //add a limit for meeting time here
+        // i set a limit for time here, the meeting should only be held from 9 - 17 on workdays
+        String timeFormat = "";
+        timeFormat += String.valueOf(yearS);
+        timeFormat += "-";
+        timeFormat += String.valueOf(monthS);
+        timeFormat += "-";
+        timeFormat += String.valueOf(dayS);
+        timeFormat += "  ";
+        timeFormat += String.valueOf(hourS);
+        timeFormat += ":";
+        timeFormat += String.valueOf(minuteS);
+        this.calendarS = Calendar.getInstance();
+        calendarS.set(yearS, monthS - 1, dayS, hourS, minuteS);
+        if ((hourS >= 9 && hourS <= 17) &&
+                (calendarS.get(Calendar.DAY_OF_WEEK) >= 2 && calendarS.get(Calendar.DAY_OF_WEEK) <= 6)) {
+            this.time_start = timeFormat;
+        } else throw new IllegalArgumentException("You should set time at work hours!");
+
+        //TODO the time of the start and end should be the same day
+        //add this limitation
+        String timeFormat1 = "";
+        timeFormat1 += String.valueOf(yearE);
+        timeFormat1 += "-";
+        timeFormat1 += String.valueOf(monthE);
+        timeFormat1 += "-";
+        timeFormat1 += String.valueOf(dayE);
+        timeFormat1 += "  ";
+        timeFormat1 += String.valueOf(hourE);
+        timeFormat1 += ":";
+        timeFormat1 += String.valueOf(minuteE);
+        this.calendarE = Calendar.getInstance();
+        calendarE.set(yearE, monthE - 1, dayE, hourE, minuteE);
+        if ((hourE >= 9 && hourE <= 17) && (calendarE.get(Calendar.DAY_OF_WEEK) >= 2 && calendarE.get(Calendar.DAY_OF_WEEK) <= 6)) {
+            if (!calendarE.before(calendarS)) {
+                this.time_end = timeFormat1;
+            } else throw new IllegalArgumentException("Invalid time set!");
+        } else throw new IllegalArgumentException("You should set time at work hours!");
+
+        this.description = description;
+        this.isOnline = isOnline;
+        this.room = "room";
+        this.platform = "platform";
+        //TODO add platform and room
+    }
+
+    public void setTitle(String title) {
+        if (title != null) {
+            this.title = title;
+        } else throw new IllegalArgumentException("The title cannot be empty!");
+    }
+
+    public void setTimeS(int year, int month, int day, int hour, int minute) {
+        // i set a limit for time here, the meeting should only be held from 9 - 17 on workdays
+        String timeFormat = "";
+        timeFormat += String.valueOf(year);
+        timeFormat += "-";
+        timeFormat += String.valueOf(month);
+        timeFormat += "-";
+        timeFormat += String.valueOf(day);
+        timeFormat += "  ";
+        timeFormat += String.valueOf(hour);
+        timeFormat += ":";
+        timeFormat += String.valueOf(minute);
+        calendarS.set(year, month - 1, day, hour, minute);
+        if ((hour >= 9 && hour <= 17) && (calendarS.get(Calendar.DAY_OF_WEEK) >= 2 && calendarS.get(Calendar.DAY_OF_WEEK) <= 6)) {
+            this.time_start = timeFormat;
+        } else throw new IllegalArgumentException("You should set time at work hours!");
+    }
+
+    public void setTimeE(int year, int month, int day, int hour, int minute) {
+        // i set a limit for time here, the meeting should only be held from 9 - 17 on workdays
+        String timeFormat = "";
+        timeFormat += String.valueOf(year);
+        timeFormat += "-";
+        timeFormat += String.valueOf(month);
+        timeFormat += "-";
+        timeFormat += String.valueOf(day);
+        timeFormat += "  ";
+        timeFormat += String.valueOf(hour);
+        timeFormat += ":";
+        timeFormat += String.valueOf(minute);
+        calendarE.set(year, month - 1, day, hour, minute);
+        if ((hour >= 9 && hour <= 17) && (calendarE.get(Calendar.DAY_OF_WEEK) >= 2 && calendarE.get(Calendar.DAY_OF_WEEK) <= 6)) {
+            if (!calendarE.before(calendarS)) {
+                this.time_end = timeFormat;
+            } else throw new IllegalArgumentException("Invalid time set!");
+        } else throw new IllegalArgumentException("You should set time at work hours!");
+    }
+
+    public void setDescription(String des) {
+        this.description = des;
+    }
+
+    public void setOnline(boolean isOnline) {
+        this.isOnline = isOnline;
+    }
+
+    public void setRoom(String room) {
+        if (isOnline) {
+            throw new IllegalArgumentException("You cannot choose a physical room if you have online meeting!");
+        } else this.room = room;
+    }
+
+    public void setPlatform(String platform) {
+        if (!isOnline) {
+            throw new IllegalArgumentException("You cannot choose an online platform if you have physical meeting!");
+        } else this.platform = platform;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public String getTime_create() {
+        return time_create;
+    }
+
+    public String getTime_start() {
+        return time_start;
+    }
+
+    public String getTime_end() {
+        return time_end;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public Calendar getCalendarS() {
+        return calendarS;
+    }
+
+    public Calendar getCalendarE() {
+        return calendarE;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + getTitle() + ", Time create: " + getTime_create() + ", Start: " + getTime_start() + ", End: " + getTime_end()
+                + ", Description: " + getDescription();
+        //should print the room or platform also
+    }
+}
---
+++ src/Event/EventList.java
@@ -0,0 +1,86 @@
+package Event;
+
+import java.util.ArrayList;
+
+public class EventList {
+    private ArrayList<Event> events;
+
+    public EventList(){
+        this.events = new ArrayList<>();
+    }
+
+    //TODO add limitation when adding event
+    public void add(Event event){
+        events.add(event);
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    //TODO the selection should be more detailed
+    //by specific date, month, year, hour
+    //and also the platform and room later
+
+    public ArrayList<Event> getEventByCreateTime(String date){
+        ArrayList<Event> eventsC = new ArrayList<>();
+        for (int i = 0; i < events.size(); i ++){
+            if (events.get(i).getTime_create().contains(date)){
+                eventsC.add(events.get(i));
+            }
+        }
+        return eventsC;
+    }
+
+    public ArrayList<Event> getEventByStartTime(String date){
+        ArrayList<Event> eventsS = new ArrayList<>();
+        for (int i = 0; i < events.size(); i++){
+            if (events.get(i).getTime_start().contains(date)){
+                eventsS.add(events.get(i));
+            }
+        }
+        return eventsS;
+    }
+
+    public ArrayList<Event> getEventByTitle(String title){
+        ArrayList<Event> eventsT = new ArrayList<>();
+        for (int i = 0; i < events.size(); i++){
+            if (events.get(i).getTitle().equals(title)){
+                eventsT.add(events.get(i));
+            }
+        }
+        return eventsT;
+    }
+
+    public Event getEventByIndex(int index){
+        return events.get(index);
+    }
+
+    public Event getEvent(Event e){
+        for (int i = 0; i < events.size(); i++){
+            if (events.get(i).equals(e)){
+                return e;
+            }
+        }
+        throw new IllegalArgumentException("No event found.");
+    }
+
+    public void remove(int index){
+        events.remove(index);
+    }
+
+    public void removeByEvent(Event e){
+        for (int i = 0; i < events.size(); i++){
+            if (events.get(i).equals(e)){
+                events.remove(e);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "EventList{" +
+                "events=" + events +
+                '}';
+    }
+}
