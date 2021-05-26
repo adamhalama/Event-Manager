f3f769f81501bbd8368bd6056f90df0f69f21985
hadamhej <janadam.h@hotmail.sk>
2021-05-26 23:23:10 +0200

    - changed ViewModelFactory returns to lean initiation, so we dont load everything at once at the start, and we load it only when needed. - but if it  wont be good, very easy to rollback

--- src/client/ViewModel/ViewModelFactory.java
+++ src/client/ViewModel/ViewModelFactory.java
@@ -9,99 +9,140 @@ import client.View.Helpers.SelectState;
 
 public class ViewModelFactory
 {
-    private final EventListViewModel eventListViewModel;
-    private final CreateEventViewModel createEventViewModel;
-    private final CreateRoomViewModel createRoomViewModel;
-    private final MainMenuViewModel mainMenuViewModel;
-    private final RoomListViewModel roomListViewModel;
-    private final EditEventViewModel editEventViewModel;
-    private final EmployeeListViewModel employeeListViewModel;
-    private final EmployeeViewModel employeeViewModel;
-    private final MessageRoomListViewModel messageRoomListViewModel;
+    private EventListViewModel eventListViewModel;
+    private CreateEventViewModel createEventViewModel;
+    private CreateRoomViewModel createRoomViewModel;
+    private MainMenuViewModel mainMenuViewModel;
+    private RoomListViewModel roomListViewModel;
+    private EditEventViewModel editEventViewModel;
+    private EmployeeListViewModel employeeListViewModel;
+    private EmployeeViewModel employeeViewModel;
+    private MessageRoomListViewModel messageRoomListViewModel;
+    private RoomViewModel roomViewModel;
+    private MessageRoomViewModel messageRoomViewModel;
+    private LoginViewModel loginViewModel;
+    private EventInfoViewModel eventInfoViewModel;
+    
     private SelectState state;
-    private final RoomViewModel roomViewModel;
-    private final MessageRoomViewModel messageRoomViewModel;
-    private final LoginViewModel loginViewModel;
-    private final EventInfoViewModel eventInfoViewModel;
+    private final Model model;
 
-
-    public ViewModelFactory(Model model, SelectState state, RmiClient api)
+    public ViewModelFactory(Model model, SelectState state)
     {
+        this.model = model;
         this.state = state;
-        eventListViewModel = new EventListViewModel(model);
-        createEventViewModel = new CreateEventViewModel(model);
-        createRoomViewModel = new CreateRoomViewModel(model);
-        mainMenuViewModel = new MainMenuViewModel(model);
-        roomListViewModel = new RoomListViewModel(model);
-        roomViewModel = new RoomViewModel(model);
-        editEventViewModel = new EditEventViewModel(model, state);
-        employeeListViewModel = new EmployeeListViewModel(model);
-        employeeViewModel = new EmployeeViewModel(model);
-        messageRoomListViewModel = new MessageRoomListViewModel(model);
-        messageRoomViewModel = new MessageRoomViewModel(model);
-        loginViewModel = new LoginViewModel(api);
-        eventInfoViewModel = new EventInfoViewModel(model, state);
+//        eventListViewModel = new EventListViewModel(model);
+//        createEventViewModel = new CreateEventViewModel(model);
+//        createRoomViewModel = new CreateRoomViewModel(model);
+//        mainMenuViewModel = new MainMenuViewModel(model);
+//        roomListViewModel = new RoomListViewModel(model);
+//        roomViewModel = new RoomViewModel(model);
+//        editEventViewModel = new EditEventViewModel(model, state);
+//        employeeListViewModel = new EmployeeListViewModel(model);
+//        employeeViewModel = new EmployeeViewModel(model);
+//        messageRoomListViewModel = new MessageRoomListViewModel(model);
+//        messageRoomViewModel = new MessageRoomViewModel(model);
+//        loginViewModel = new LoginViewModel(api);
+//        eventInfoViewModel = new EventInfoViewModel(model, state);
     }
+    public EventListViewModel getEventListViewModel()
+    {
+        if (eventListViewModel == null)
+            eventListViewModel = new EventListViewModel(model);
 
+        return eventListViewModel;
+    }
+    
     public CreateEventViewModel getCreateEventViewModel()
     {
+        if (createEventViewModel == null)
+            createEventViewModel = new CreateEventViewModel(model);
+        
         return createEventViewModel;
     }
 
     public CreateRoomViewModel getCreateRoomViewModel()
     {
+        if (createRoomViewModel == null)
+            createRoomViewModel = new CreateRoomViewModel(model);
+
         return createRoomViewModel;
     }
-
-    public EventListViewModel getEventListViewModel()
-    {
-        return eventListViewModel;
-    }
+    
 
     public MainMenuViewModel getMainMenuViewModel()
     {
+        if (mainMenuViewModel == null)
+            mainMenuViewModel = new MainMenuViewModel(model);
+
         return mainMenuViewModel;
     }
 
     public RoomListViewModel getRoomListViewModel()
     {
+        if (roomListViewModel == null)
+            roomListViewModel = new RoomListViewModel(model);
+
         return roomListViewModel;
     }
 
     public RoomViewModel getRoomViewModel()
     {
+        if (roomViewModel == null)
+            roomViewModel = new RoomViewModel(model);
+
         return roomViewModel;
     }
 
     public EditEventViewModel getEditEventViewModel() {
+        if (editEventViewModel == null)
+            editEventViewModel = new EditEventViewModel(model, state);
+
         return editEventViewModel;
     }
 
     public EmployeeListViewModel getEmployeeListViewModel()
     {
+        if (employeeListViewModel == null)
+            employeeListViewModel = new EmployeeListViewModel(model);
+
         return employeeListViewModel;
     }
 
     public EmployeeViewModel getEmployeeViewModel()
     {
+        if (employeeViewModel == null)
+            employeeViewModel = new EmployeeViewModel(model);
+
         return employeeViewModel;
     }
 
     public MessageRoomListViewModel getMessageRoomListViewModel()
     {
+        if (messageRoomListViewModel == null)
+            messageRoomListViewModel = new MessageRoomListViewModel(model);
+
         return messageRoomListViewModel;
     }
 
     public MessageRoomViewModel getMessageRoomViewModel()
     {
+        if (messageRoomViewModel == null)
+            messageRoomViewModel = new MessageRoomViewModel(model);
+
         return messageRoomViewModel;
     }
 
     public LoginViewModel getLoginViewModel() {
+        if (loginViewModel == null)
+            loginViewModel = new LoginViewModel(model);
+
         return loginViewModel;
     }
 
     public EventInfoViewModel getEventInfoViewModel() {
+        if (eventInfoViewModel == null)
+            eventInfoViewModel = new EventInfoViewModel(model, state);
+
         return eventInfoViewModel;
     }
 }
