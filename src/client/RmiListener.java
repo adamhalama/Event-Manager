cd77856e970fe84d68af87eb30dc32f6f7cd4531
Jack Hamer <jackhamer0990@gmail.com>
2021-06-01 20:38:46 +0200

    Update RmiListener.java

--- src/client/RmiListener.java
+++ src/client/RmiListener.java
@@ -1,6 +1,11 @@
 package client;
 
 import Shared.ClientListener;
+import Shared.Employee.Employee;
+import Shared.Event.Event;
+import Shared.Messages.Message;
+import Shared.Messages.MessageRoom;
+import Shared.Room.Room;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -12,7 +17,27 @@ public class RmiListener implements ClientListener
   }
 
   @Override
-  public void doSomething(String asd) throws RemoteException {
-    System.out.println("Server invoked doSomething()");
-  }
+  public void employeeUpdate(Employee employee) throws RemoteException {}
+  @Override
+  public void employeeDelete(int employeeID) throws RemoteException {}
+
+  @Override
+  public void roomUpdate(Room room) throws RemoteException {}
+  @Override
+  public void roomDelete(int roomID) throws RemoteException {}
+
+  @Override
+  public void roomEquipmentUpdate(int roomID, String[] equipment) throws RemoteException {}
+
+  @Override
+  public void messageRoomUpdate(MessageRoom messageRoom) throws RemoteException {}
+  @Override
+  public void messageRoomDelete(int messageRoomID) throws RemoteException {}
+  @Override
+  public void messageRoomNotify(int messageRoomID, Message message) throws RemoteException {}
+
+  @Override
+  public void eventUpdate(Event event) throws RemoteException {}
+  @Override
+  public void eventDelete(int eventID) throws RemoteException {}
 }
