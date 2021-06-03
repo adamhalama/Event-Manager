b13d508166647b589892b4ff1f2c18464f631972
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2021-06-03 23:01:01 +0200

    add the javadoc for Event

--- src/Shared/Event/Event.java
+++ src/Shared/Event/Event.java
@@ -7,16 +7,49 @@ import java.util.ArrayList;
 
 public class Event implements Serializable
 {
+    /**
+     * An integer for event id.
+     */
     private int id;
+    /**
+     * An integer for message room if.
+     */
     private int messageRoomID;
+    /**
+     * An integer for physical room id.
+     */
     private int roomID;
+    /**
+     * An integer for creator employee if.
+     */
     private int creatorID;
+    /**
+     * A long integer storing the timestamp of start time.
+     */
     private long timeStart;
+    /**
+     * A long integer storing the timestamp of end time.
+     */
     private long timeEnd;
+    /**
+     * A string storing title of the event.
+     */
     private String title;
+    /**
+     * A string containing the description of the event.
+     */
     private String description;
+    /**
+     * A string containing the online platform of the event.
+     */
     private String platform;
+    /**
+     * A string containing the online platform's link of the event.
+     */
     private String onlineLink;
+    /**
+     * An arraylist storing all the participants' ids.
+     */
     private ArrayList<Integer> participants;
 
     /**
@@ -49,28 +82,19 @@ public class Event implements Serializable
         this.participants = participants;
     }
 
-    public void addParticipant(int id)
-    {
-        participants.add(id);
-    }
-
-    public void removeParticipant(int id)
-    {
-        for (int i = 0; i < participants.size(); i++)
-        {
-            if (participants.get(i) == id)
-            {
-                participants.remove(i);
-                break;
-            }
-        }
-    }
-
+    /**
+     * Gets event's ID.
+     * @return An integer storing event's ID.
+     */
     public int getID()
     {
         return id;
     }
 
+    /**
+     * Sets the event's ID.
+     * @param id An integer storing event's ID.
+     */
     public void setID(int id)
     {
         if (id != -1)
@@ -79,11 +103,19 @@ public class Event implements Serializable
         }
     }
 
+    /**
+     * Gets the message room ID.
+     * @return An integer storing message room ID.
+     */
     public int getMessageRoomID()
     {
         return messageRoomID;
     }
 
+    /**
+     * Sets the message room ID.
+     * @param messageRoomID An integer storing message room ID.
+     */
     public void setMessageRoomID(int messageRoomID)
     {
         if (messageRoomID != -1)
@@ -92,11 +124,19 @@ public class Event implements Serializable
         }
     }
 
+    /**
+     * Gets the physical room ID.
+     * @return An integer storing physical room ID.
+     */
     public int getRoomID()
     {
         return roomID;
     }
 
+    /**
+     * Sets the physical room ID.
+     * @param roomID An integer storing physical room ID.
+     */
     public void setRoomID(int roomID)
     {
         if (roomID != -1)
@@ -105,41 +145,55 @@ public class Event implements Serializable
         }
     }
 
+    /**
+     * Gets the creator's employeeID.
+     * @return An integer storing creator's employeeID.
+     */
     public int getCreatorID()
     {
         return creatorID;
     }
 
+    /**
+     * Gets the timestamp of the start time.
+     * @return A long integer of the timestamp of start time.
+     */
     public long getTimeStart()
     {
         return timeStart;
     }
 
-    public void setTimeStart(long timestamp)
-    {
-        this.timeStart = timestamp;
-    }
-
+    /**
+     * Gets the timestamp of the end time.
+     * @return A long integer of the timestamp of end time.
+     */
     public long getTimeEnd()
     {
         return timeEnd;
     }
 
-    public void setTimeEnd(long timestamp)
-    {
-        this.timeEnd = timestamp;
-    }
-
+    /**
+     * Gets both start time and end time.
+     * @return A string storing the converted start time and end time.
+     */
     public String getTimeStartEnd()
     {
         return ConvertTime.getFormattedDateTime(timeStart) + " - " + ConvertTime.getFormattedTime(timeEnd);
     }
 
+    /**
+     * Gets the event's title.
+     * @return A string storing the title.
+     */
     public String getTitle()
     {
         return title;
     }
 
+    /**
+     * Sets title of the event.
+     * @param title A string storing the title.
+     */
     public void setTitle(String title)
     {
         if (title != null)
@@ -148,41 +202,64 @@ public class Event implements Serializable
         } else throw new IllegalArgumentException("The title cannot be empty!");
     }
 
+    /**
+     * Gets description of the event.
+     * @return A string storing the description.
+     */
     public String getDescription()
     {
         return description;
     }
 
+    /**
+     * Sets description of the event.
+     * @param description A string storing the description.
+     */
     public void setDescription(String description)
     {
         this.description = description;
     }
 
+    /**
+     * Gets the online platform.
+     * @return A string storing the platform.
+     */
     public String getPlatform()
     {
         return platform;
     }
 
+    /**
+     * Sets the online platform.
+     * @param platform A string storing the platform.
+     */
     public void setPlatform(String platform)
     {
         this.platform = platform;
     }
 
+    /**
+     * Gets the online link of online meeting.
+     * @return A string storing the link.
+     */
     public String getOnlineLink()
     {
         return onlineLink;
     }
 
-    public void setOnlineLink(String link)
-    {
-        this.onlineLink = link;
-    }
-
+    /**
+     * Gets all the participants in the event.
+     * @return An arraylist containing all the participants.
+     */
     public ArrayList<Integer> getParticipants()
     {
         return participants;
     }
 
+    /**
+     * Sets all the participants in the event.
+     * @param employees An arraylist containing all the participants..
+     */
     public void setParticipants(ArrayList<Integer> employees)
     {
         this.participants = employees;
